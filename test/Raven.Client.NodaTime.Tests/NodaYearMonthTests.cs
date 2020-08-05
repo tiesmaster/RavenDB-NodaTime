4b37d1bb63a3aea7a6b75119c7b430bb763476fb
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-08-05 21:26:24 +0200

    Switch LocalDate in NodaYearMonthTests -> YearMonth

--- test/Raven.Client.NodaTime.Tests/NodaYearMonthTests.cs
+++ test/Raven.Client.NodaTime.Tests/NodaYearMonthTests.cs
@@ -8,33 +8,33 @@ using Xunit;
 
 namespace Raven.Client.NodaTime.Tests
 {
-    public class NodaLocalDateTests : MyRavenTestDriver
+    public class NodaYearMonthTests : MyRavenTestDriver
     {
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Document_Today()
+        public void Can_Use_NodaTime_YearMonth_In_Document_Today()
         {
-            Can_Use_NodaTime_LocalDate_In_Document(NodaUtil.LocalDate.Today);
+            Can_Use_NodaTime_YearMonth_In_Document(NodaUtil.YearMonth.Today);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Document_IsoMin()
+        public void Can_Use_NodaTime_YearMonth_In_Document_IsoMin()
         {
-            Can_Use_NodaTime_LocalDate_In_Document(NodaUtil.LocalDate.MinIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Document(NodaUtil.YearMonth.MinIsoValue);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Document_IsoMax()
+        public void Can_Use_NodaTime_YearMonth_In_Document_IsoMax()
         {
-            Can_Use_NodaTime_LocalDate_In_Document(NodaUtil.LocalDate.MaxIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Document(NodaUtil.YearMonth.MaxIsoValue);
         }
 
-        private void Can_Use_NodaTime_LocalDate_In_Document(LocalDate ld)
+        private void Can_Use_NodaTime_YearMonth_In_Document(YearMonth ld)
         {
             using (var documentStore = NewDocumentStore())
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
+                    session.Store(new Foo { Id = "foos/1", YearMonth = ld });
                     session.SaveChanges();
                 }
 
@@ -42,7 +42,7 @@ namespace Raven.Client.NodaTime.Tests
                 {
                     var foo = session.Load<Foo>("foos/1");
 
-                    Assert.Equal(ld, foo.LocalDate);
+                    Assert.Equal(ld, foo.YearMonth);
                 }
 
                 using (var session = documentStore.OpenSession())
@@ -51,124 +51,124 @@ namespace Raven.Client.NodaTime.Tests
                     session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
                     var json = (BlittableJsonReaderObject)command.Result.Results[0];
                     System.Diagnostics.Debug.WriteLine(json.ToString());
-                    var expected = ld.ToString(LocalDatePattern.Iso.PatternText, null);
-                    json.TryGet("LocalDate", out string value);
+                    var expected = ld.ToString(YearMonthPattern.Iso.PatternText, null);
+                    json.TryGet("YearMonth", out string value);
                     Assert.Equal(expected, value);
                 }
             }
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Dynamic_Index_Today()
+        public void Can_Use_NodaTime_YearMonth_In_Dynamic_Index_Today()
         {
-            Can_Use_NodaTime_LocalDate_In_Dynamic_Index1(NodaUtil.LocalDate.Today);
+            Can_Use_NodaTime_YearMonth_In_Dynamic_Index1(NodaUtil.YearMonth.Today);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Dynamic_Index_IsoMin()
+        public void Can_Use_NodaTime_YearMonth_In_Dynamic_Index_IsoMin()
         {
-            Can_Use_NodaTime_LocalDate_In_Dynamic_Index1(NodaUtil.LocalDate.MinIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Dynamic_Index1(NodaUtil.YearMonth.MinIsoValue);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Dynamic_Index_IsoMax()
+        public void Can_Use_NodaTime_YearMonth_In_Dynamic_Index_IsoMax()
         {
-            Can_Use_NodaTime_LocalDate_In_Dynamic_Index2(NodaUtil.LocalDate.MaxIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Dynamic_Index2(NodaUtil.YearMonth.MaxIsoValue);
         }
 
-        private void Can_Use_NodaTime_LocalDate_In_Dynamic_Index1(LocalDate ld)
+        private void Can_Use_NodaTime_YearMonth_In_Dynamic_Index1(YearMonth ld)
         {
             using (var documentStore = NewDocumentStore())
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
-                    session.Store(new Foo { Id = "foos/2", LocalDate = ld + Period.FromDays(1) });
-                    session.Store(new Foo { Id = "foos/3", LocalDate = ld + Period.FromDays(2) });
+                    session.Store(new Foo { Id = "foos/1", YearMonth = ld });
+                    session.Store(new Foo { Id = "foos/2", YearMonth = ld + Period.FromDays(1) });
+                    session.Store(new Foo { Id = "foos/3", YearMonth = ld + Period.FromDays(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate == ld);
+                                    .Where(x => x.YearMonth == ld);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate > ld)
-                                    .OrderByDescending(x => x.LocalDate);
+                                    .Where(x => x.YearMonth > ld)
+                                    .OrderByDescending(x => x.YearMonth);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].LocalDate > results2[1].LocalDate);
+                    Assert.True(results2[0].YearMonth > results2[1].YearMonth);
 
                     var q3 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate >= ld)
-                                    .OrderByDescending(x => x.LocalDate);
+                                    .Where(x => x.YearMonth >= ld)
+                                    .OrderByDescending(x => x.YearMonth);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].LocalDate > results3[1].LocalDate);
-                    Assert.True(results3[1].LocalDate > results3[2].LocalDate);
+                    Assert.True(results3[0].YearMonth > results3[1].YearMonth);
+                    Assert.True(results3[1].YearMonth > results3[2].YearMonth);
                 }
             }
         }
 
-        private void Can_Use_NodaTime_LocalDate_In_Dynamic_Index2(LocalDate ld)
+        private void Can_Use_NodaTime_YearMonth_In_Dynamic_Index2(YearMonth ld)
         {
             using (var documentStore = NewDocumentStore())
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
-                    session.Store(new Foo { Id = "foos/2", LocalDate = ld - Period.FromDays(1) });
-                    session.Store(new Foo { Id = "foos/3", LocalDate = ld - Period.FromDays(2) });
+                    session.Store(new Foo { Id = "foos/1", YearMonth = ld });
+                    session.Store(new Foo { Id = "foos/2", YearMonth = ld - Period.FromDays(1) });
+                    session.Store(new Foo { Id = "foos/3", YearMonth = ld - Period.FromDays(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate == ld);
+                                    .Where(x => x.YearMonth == ld);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate < ld)
-                                    .OrderBy(x => x.LocalDate);
+                                    .Where(x => x.YearMonth < ld)
+                                    .OrderBy(x => x.YearMonth);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].LocalDate < results2[1].LocalDate);
+                    Assert.True(results2[0].YearMonth < results2[1].YearMonth);
 
                     var q3 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate <= ld)
-                                    .OrderBy(x => x.LocalDate);
+                                    .Where(x => x.YearMonth <= ld)
+                                    .OrderBy(x => x.YearMonth);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].LocalDate < results3[1].LocalDate);
-                    Assert.True(results3[1].LocalDate < results3[2].LocalDate);
+                    Assert.True(results3[0].YearMonth < results3[1].YearMonth);
+                    Assert.True(results3[1].YearMonth < results3[2].YearMonth);
                 }
             }
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Static_Index_Today()
+        public void Can_Use_NodaTime_YearMonth_In_Static_Index_Today()
         {
-            Can_Use_NodaTime_LocalDate_In_Static_Index1(NodaUtil.LocalDate.Today);
+            Can_Use_NodaTime_YearMonth_In_Static_Index1(NodaUtil.YearMonth.Today);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Static_Index_IsoMin()
+        public void Can_Use_NodaTime_YearMonth_In_Static_Index_IsoMin()
         {
-            Can_Use_NodaTime_LocalDate_In_Static_Index1(NodaUtil.LocalDate.MinIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Static_Index1(NodaUtil.YearMonth.MinIsoValue);
         }
 
         [Fact]
-        public void Can_Use_NodaTime_LocalDate_In_Static_Index_IsoMax()
+        public void Can_Use_NodaTime_YearMonth_In_Static_Index_IsoMax()
         {
-            Can_Use_NodaTime_LocalDate_In_Static_Index2(NodaUtil.LocalDate.MaxIsoValue);
+            Can_Use_NodaTime_YearMonth_In_Static_Index2(NodaUtil.YearMonth.MaxIsoValue);
         }
 
-        private void Can_Use_NodaTime_LocalDate_In_Static_Index1(LocalDate ld)
+        private void Can_Use_NodaTime_YearMonth_In_Static_Index1(YearMonth ld)
         {
             using (var documentStore = NewDocumentStore())
             {
@@ -176,38 +176,38 @@ namespace Raven.Client.NodaTime.Tests
 
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
-                    session.Store(new Foo { Id = "foos/2", LocalDate = ld + Period.FromDays(1) });
-                    session.Store(new Foo { Id = "foos/3", LocalDate = ld + Period.FromDays(2) });
+                    session.Store(new Foo { Id = "foos/1", YearMonth = ld });
+                    session.Store(new Foo { Id = "foos/2", YearMonth = ld + Period.FromDays(1) });
+                    session.Store(new Foo { Id = "foos/3", YearMonth = ld + Period.FromDays(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate == ld);
+                                    .Where(x => x.YearMonth == ld);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate > ld)
-                                    .OrderByDescending(x => x.LocalDate);
+                                    .Where(x => x.YearMonth > ld)
+                                    .OrderByDescending(x => x.YearMonth);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].LocalDate > results2[1].LocalDate);
+                    Assert.True(results2[0].YearMonth > results2[1].YearMonth);
 
                     var q3 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate >= ld)
-                                    .OrderByDescending(x => x.LocalDate);
+                                    .Where(x => x.YearMonth >= ld)
+                                    .OrderByDescending(x => x.YearMonth);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].LocalDate > results3[1].LocalDate);
-                    Assert.True(results3[1].LocalDate > results3[2].LocalDate);
+                    Assert.True(results3[0].YearMonth > results3[1].YearMonth);
+                    Assert.True(results3[1].YearMonth > results3[2].YearMonth);
                 }
             }
         }
 
-        private void Can_Use_NodaTime_LocalDate_In_Static_Index2(LocalDate ld)
+        private void Can_Use_NodaTime_YearMonth_In_Static_Index2(YearMonth ld)
         {
             using (var documentStore = NewDocumentStore())
             {
@@ -215,33 +215,33 @@ namespace Raven.Client.NodaTime.Tests
 
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
-                    session.Store(new Foo { Id = "foos/2", LocalDate = ld - Period.FromDays(1) });
-                    session.Store(new Foo { Id = "foos/3", LocalDate = ld - Period.FromDays(2) });
+                    session.Store(new Foo { Id = "foos/1", YearMonth = ld });
+                    session.Store(new Foo { Id = "foos/2", YearMonth = ld - Period.FromDays(1) });
+                    session.Store(new Foo { Id = "foos/3", YearMonth = ld - Period.FromDays(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate == ld);
+                                    .Where(x => x.YearMonth == ld);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate < ld)
-                                    .OrderBy(x => x.LocalDate);
+                                    .Where(x => x.YearMonth < ld)
+                                    .OrderBy(x => x.YearMonth);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].LocalDate < results2[1].LocalDate);
+                    Assert.True(results2[0].YearMonth < results2[1].YearMonth);
 
                     var q3 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.LocalDate <= ld)
-                                    .OrderBy(x => x.LocalDate);
+                                    .Where(x => x.YearMonth <= ld)
+                                    .OrderBy(x => x.YearMonth);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].LocalDate < results3[1].LocalDate);
-                    Assert.True(results3[1].LocalDate < results3[2].LocalDate);
+                    Assert.True(results3[0].YearMonth < results3[1].YearMonth);
+                    Assert.True(results3[1].YearMonth < results3[2].YearMonth);
                 }
             }
         }
@@ -249,7 +249,7 @@ namespace Raven.Client.NodaTime.Tests
         public class Foo
         {
             public string Id { get; set; }
-            public LocalDate LocalDate { get; set; }
+            public YearMonth YearMonth { get; set; }
         }
 
         public class TestIndex : AbstractIndexCreationTask<Foo>
@@ -259,7 +259,7 @@ namespace Raven.Client.NodaTime.Tests
                 Map = foos => from foo in foos
                               select new
                               {
-                                  foo.LocalDate
+                                  foo.YearMonth
                               };
             }
         }
